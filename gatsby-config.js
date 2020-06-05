891297b2778ca721931b586bce52cdb63b9beb52
Tanweer Anwar <tanweer.anwar919@gmail.com>
2020-06-05 17:52:47 +0530

    Added gatsby source filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,23 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
+  plugins: [
+    {
+      resolve : 'gatsby-source-filesystem',
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    },
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography";
+import kirkhamTheme from "typography-theme-kirkham";
+
+const typography = new Typography(kirkhamTheme);
+export const { rhythm } = typography; 
+export default typography;
